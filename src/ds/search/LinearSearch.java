a3ba9cbee1fedcb4e40aeb25dde9d67d2125ff19
Subhash Reddy <subhashreddy22@gmail.com>
2021-12-21 19:22:40 +0530

    liner search added

---
+++ src/ds/search/LinearSearch.java
@@ -0,0 +1,38 @@
+package ds.search;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter element to search");
+        int x = sc.nextInt();
+
+        int i = search(arr, x);
+
+        if (i == -1) {
+            System.out.println("Element entered is not found");
+        } else {
+            System.out.println("Element found at index " + i);
+        }
+    }
+
+    static int search(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
